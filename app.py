27a894ea270102e0a6710b335277e51fa345f7b0
Saurabh sanjay yeole <155825578+beaming1232@users.noreply.github.com>
2025-06-10 10:09:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,38 @@
-from flask import Flask,request, render_template
+from flask import Flask, request, jsonify
 import numpy as np
 import pickle
 import sklearn
-print(sklearn.__version__)
-#loading models
-dtr = pickle.load(open('dtr.pkl','rb'))
-preprocessor = pickle.load(open('preprocessor.pkl','rb'))
 
-#flask app
+print(sklearn.__version__)
+
+# Loading models
+dtr = pickle.load(open('dtr.pkl', 'rb'))
+preprocessor = pickle.load(open('preprocessor.pkl', 'rb'))
+
+# Flask app
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-@app.route("/predict",methods=['POST'])
+    return jsonify({"message": "Server is running"})
+
+@app.route("/predict", methods=['POST'])
 def predict():
-    if request.method == 'POST':
-        Year = request.form['Year']
-        average_rain_fall_mm_per_year = request.form['average_rain_fall_mm_per_year']
-        pesticides_tonnes = request.form['pesticides_tonnes']
-        avg_temp = request.form['avg_temp']
-        Area = request.form['Area']
-        Item  = request.form['Item']
+    data = request.form
+    Year = data['Year']
+    average_rain_fall_mm_per_year = data['average_rain_fall_mm_per_year']
+    pesticides_tonnes = data['pesticides_tonnes']
+    avg_temp = data['avg_temp']
+    Area = data['Area']
+    Item = data['Item']
 
-        features = np.array([[Year,average_rain_fall_mm_per_year,pesticides_tonnes,avg_temp,Area,Item]],dtype=object)
-        transformed_features = preprocessor.transform(features)
-        prediction = dtr.predict(transformed_features).reshape(1,-1)
+    features = np.array([[Year, average_rain_fall_mm_per_year, pesticides_tonnes, avg_temp, Area, Item]], dtype=object)
+    transformed_features = preprocessor.transform(features)
+    prediction = dtr.predict(transformed_features).reshape(1, -1)
 
-        return render_template('index.html',prediction = prediction[0][0])
+    return jsonify({"prediction": float(prediction[0][0])})
 
-if __name__=="__main__":
-    app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
